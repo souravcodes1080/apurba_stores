b8651fb46fa06a5b6552b9515898b82c9ed7ec9c
souravcodes1080 <souravch2003@gmail.com>
2023-09-13 12:16:25 +0530

    popup added

---
+++ script.js
@@ -0,0 +1,10 @@
+const popup = document.querySelector(".popup");
+const cross = document.querySelector(".cross");
+
+cross.addEventListener("click", ()=>{
+    popup.style.bottom = "-300px";
+})
+
+setTimeout(()=>{
+    popup.style.bottom = "20px";
+}, 5000)
